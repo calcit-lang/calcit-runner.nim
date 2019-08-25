de8043d5c32247d7b819a24484c635ba7a96337b
jiyinyiyong <jiyinyiyong@gmail.com>
2019-08-26 01:23:02 +0800

    splitting types file

--- src/main.nim
+++ src/main.nim
@@ -4,33 +4,7 @@ import cirruParser
 import sequtils
 from strutils import join
 import strformat
-
-type InterpretError = Exception
-
-type
-  CirruValueKind = enum
-    crValueInt,
-    crValueFloat,
-    crValueString,
-    crValueVector,
-    crValueSeq,
-    crValueMap,
-    crValueFn
-
-  CirruValue = object
-    case kind: CirruValueKind
-    of crValueInt: intVal: int
-    of crValueFloat: floatVal: float
-    of crValueString: stringVal: string
-    of crValueFn: fnVal: proc()
-    else: xVal: string
-
-proc toString(val: CirruValue): string =
-  case val.kind:
-    of crValueInt: $(val.intVal)
-    of crValueFloat: $(val.floatVal)
-    of crValueString: val.stringVal
-    else: "CirruValue"
+import cirruInterpreter/types
 
 proc interpret(expr: CirruNode): CirruValue =
   if expr.kind == cirruString:
