d927866095f7aaf3755e1b605d1bbe7080ead5d6
jiyinyiyong <jiyinyiyong@gmail.com>
2019-10-05 02:31:13 +0800

    extremely naive work on hash tables

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 
 # build and run example
 # nim compile --verbosity:0 --hints:off -r src/cr example/echo.cirru
-nim compile --verbosity:0 --hints:off -r src/cr example/array.cirru
+# nim compile --verbosity:0 --hints:off -r src/cr example/array.cirru
+nim compile --verbosity:0 --hints:off -r src/cr example/table.cirru
