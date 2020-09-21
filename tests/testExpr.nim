37c4b3c7d2667710d380fcc66d7074e3602119af
jiyinyiyong <jiyinyiyong@gmail.com>
2020-09-21 12:25:26 +0800

    test hygienic macro add

--- tests/testExpr.nim
+++ tests/testExpr.nim
@@ -5,3 +5,6 @@ import calcitRunner
 
 test "Basic add":
   check (runProgram("tests/snapshots/add.cirru") == crData(3))
+
+test "Basic gynienic":
+  check (runProgram("tests/snapshots/gynienic.cirru") == crData(14))
