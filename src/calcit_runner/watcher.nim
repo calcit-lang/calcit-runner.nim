9b6815e82504222334917d5db20cbdd10edf681d
jiyinyiyong <jiyinyiyong@gmail.com>
2020-12-25 20:02:09 +0800

    apply-args

--- src/calcit_runner/watcher.nim
+++ src/calcit_runner/watcher.nim
@@ -5,7 +5,6 @@ import libfswatch/fswatch
 var watchingChan*: Channel[string]
 
 proc watchingTask*(incrementFile: string) {.thread.} =
-  echo "watching..."
   let fileChangeCb = proc (event: fsw_cevent, event_num: cuint): void =
     watchingChan.send("changed")
 
