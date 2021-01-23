2f10da65869136d83b276568c0b6fa16dbec772e
jiyinyiyong <jiyinyiyong@gmail.com>
2021-01-23 14:28:08 +0800

    move ts/webpack configs to project root

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ let bundleTarget = process.env.target === "node" ? 'node': 'web';
 console.log("bundle mode:", bundleTarget);
 
 module.exports = {
-  entry: "./main.mjs",
+  entry: "./js-out/main.mjs",
   target: bundleTarget,
   mode: "development",
   devtool: "hidden-source-map",
   output: {
-    path: path.resolve(__dirname, "./"),
+    path: path.resolve(__dirname, "./js-out/"),
     filename: "bundle.js",
   },
 };
