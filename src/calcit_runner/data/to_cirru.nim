2366bdc213ea226523b9c00768c6bf450326f662
jiyinyiyong <jiyinyiyong@gmail.com>
2021-01-25 16:04:13 +0800

    fixes since breaking deps in cirru_parser

--- src/calcit_runner/data/to_cirru.nim
+++ src/calcit_runner/data/to_cirru.nim
@@ -11,26 +11,26 @@ import ../util/errors
 proc toCirruNode*(x: CirruData): CirruNode =
   case x.kind
   of crDataNil:
-    return CirruNode(kind: cirruString, text: "nil")
+    return CirruNode(kind: cirruToken, token: "nil")
   of crDataBool:
-    return CirruNode(kind: cirruString, text: $x.boolVal)
+    return CirruNode(kind: cirruToken, token: $x.boolVal)
   of crDataNumber:
-    return CirruNode(kind: cirruString, text: $x.numberVal)
+    return CirruNode(kind: cirruToken, token: $x.numberVal)
   of crDataString:
-    return CirruNode(kind: cirruString, text: "|" & x.stringVal)
+    return CirruNode(kind: cirruToken, token: "|" & x.stringVal)
   of crDataKeyword:
-    return CirruNode(kind: cirruString, text: ":" & x.keywordVal)
+    return CirruNode(kind: cirruToken, token: ":" & x.keywordVal)
   of crDataList:
     var elems: DoublyLinkedList[CirruNode]
     for child in x.listVal:
       elems.append toCirruNode(child)
-    return CirruNode(kind: cirruSeq, list: elems)
+    return CirruNode(kind: cirruList, list: elems)
 
   of crDataSymbol:
     # not implement symbol in cirru-edn
-    return CirruNode(kind: cirruString, text: x.symbolVal)
+    return CirruNode(kind: cirruToken, token: x.symbolVal)
   of crDataTernary:
-    return CirruNode(kind: cirruString, text: $x.ternaryVal)
+    return CirruNode(kind: cirruToken, token: $x.ternaryVal)
 
   of crDataSet, crDataMap, crDataFn, crDataProc, crDataMacro, crDataSyntax, crDataRecur, crDataAtom:
     raiseEvalError("Unexpect set to convert to CirruNode: ", x)
@@ -38,8 +38,8 @@ proc toCirruNode*(x: CirruData): CirruNode =
     raiseEvalError("must calculate thunk before converting", x)
 
 proc nodesToCirruData*(xs: CirruNode, ns: string): CirruData =
-  if xs.kind == cirruString:
-    parseLiteral(xs.text, ns)
+  if xs.kind == cirruToken:
+    parseLiteral(xs.token, ns)
   else:
     var list = initTernaryTreeList[CirruData](@[])
     for x in xs:
@@ -48,8 +48,8 @@ proc nodesToCirruData*(xs: CirruNode, ns: string): CirruData =
 
 # nodes using bare string
 proc toCirruNodesData*(xs: CirruNode): CirruData =
-  if xs.kind == cirruString:
-    CirruData(kind: crDataString, stringVal: xs.text)
+  if xs.kind == cirruToken:
+    CirruData(kind: crDataString, stringVal: xs.token)
   else:
     var list = initTernaryTreeList[CirruData](@[])
     for x in xs:
