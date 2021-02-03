16635cf59c1e0189da2704eb954e2e5214a52f51
jiyinyiyong <jiyinyiyong@gmail.com>
2021-02-03 10:10:48 +0800

    calcit-js: just split the file calcit.props.ts

--- lib/calcit.procs.ts
+++ lib/calcit.procs.ts
@@ -1,393 +1,23 @@
-import {
-  TernaryTreeList,
-  TernaryTreeMap,
-  overwriteComparator,
-  initTernaryTreeList,
-  initTernaryTreeMap,
-  listLen,
-  mapLen,
-  listGet,
-  mapGet,
-  assocMap,
-  assocList,
-  dissocMap,
-  isMapEmpty,
-  toPairs,
-  contains,
-  listToItems,
-  dissocList,
-  Hash,
-  overwriteHashGenerator,
-  valueHash,
-  mergeValueHash,
-  toPairsArray,
-} from "@calcit/ternary-tree";
+import { overwriteComparator, initTernaryTreeMap } from "@calcit/ternary-tree";
 
-import * as ternaryTree from "@calcit/ternary-tree";
+import {
+  CrDataSymbol,
+  CrDataValue,
+  CrDataKeyword,
+  CrDataList,
+  CrDataMap,
+  CrDataAtom,
+  CrDataFn,
+  CrDataRecur,
+  kwd,
+  atomsRegistry,
+  toString,
+} from "./calcit-data";
+
+export * from "./calcit-data";
 
 let inNodeJs =
   typeof process !== "undefined" && process?.release?.name === "node";
-
-class CrDataKeyword {
-  value: string;
-  cachedHash: Hash;
-  constructor(x: string) {
-    this.value = x;
-  }
-  toString() {
-    return `:${this.value}`;
-  }
-}
-export class CrDataSymbol {
-  value: string;
-  cachedHash: Hash;
-  constructor(x: string) {
-    this.value = x;
-  }
-  toString() {
-    return `'${this.value}`;
-  }
-}
-
-class CrDataRecur {
-  args: CrDataValue[];
-  constructor(xs: CrDataValue[]) {
-    this.args = xs;
-  }
-
-  toString() {
-    return `(&recur ...)`;
-  }
-}
-
-class CrDataAtom {
-  value: CrDataValue;
-  path: string;
-  listeners: Map<CrDataValue, CrDataFn>;
-  cachedHash: Hash;
-  constructor(x: CrDataValue, path: string) {
-    this.value = x;
-    this.path = path;
-    this.listeners = new Map();
-  }
-  toString(): string {
-    return `(&atom ${this.value.toString()})`;
-  }
-}
-
-type CrDataFn = (...xs: CrDataValue[]) => CrDataValue;
-
-class CrDataList {
-  value: TernaryTreeList<CrDataValue>;
-  // array mode store bare array for performance
-  arrayValue: Array<CrDataValue>;
-  arrayMode: boolean;
-  arrayStart: number;
-  arrayEnd: number;
-  cachedHash: Hash;
-  constructor(value: Array<CrDataValue> | TernaryTreeList<CrDataValue>) {
-    if (Array.isArray(value)) {
-      this.arrayMode = true;
-      this.arrayValue = value;
-      this.arrayStart = 0;
-      this.arrayEnd = value.length;
-      this.value = null;
-    } else {
-      this.arrayMode = false;
-      this.value = value;
-      this.arrayValue = [];
-      this.arrayStart = null;
-      this.arrayEnd = null;
-    }
-  }
-  turnListMode() {
-    if (this.arrayMode) {
-      this.value = initTernaryTreeList(
-        this.arrayValue.slice(this.arrayStart, this.arrayEnd)
-      );
-      this.arrayValue = null;
-      this.arrayStart = null;
-      this.arrayEnd = null;
-      this.arrayMode = false;
-    }
-  }
-  len() {
-    if (this.arrayMode) {
-      return this.arrayEnd - this.arrayStart;
-    } else {
-      return listLen(this.value);
-    }
-  }
-  get(idx: number) {
-    if (this.arrayMode) {
-      return this.arrayValue[this.arrayStart + idx];
-    } else {
-      return listGet(this.value, idx);
-    }
-  }
-  assoc(idx: number, v: CrDataValue) {
-    this.turnListMode();
-    return new CrDataList(assocList(this.value, idx, v));
-  }
-  dissoc(idx: number) {
-    this.turnListMode();
-    return new CrDataList(dissocList(this.value, idx));
-  }
-  slice(from: number, to: number) {
-    if (this.arrayMode) {
-      if (from < 0) {
-        throw new Error(`from index too small: ${from}`);
-      }
-      if (to > this.len()) {
-        throw new Error(`end index too large: ${to}`);
-      }
-      let result = new CrDataList(this.arrayValue);
-      result.arrayStart = this.arrayStart + from;
-      result.arrayEnd = this.arrayStart + to;
-      return result;
-    } else {
-      return new CrDataList(ternaryTree.slice(this.value, from, to));
-    }
-  }
-  toString() {
-    let result = "";
-    for (let item of this.items()) {
-      if (result !== "") {
-        result = `${result}, `;
-      }
-      result = `${result}${toString(item, true)}`;
-    }
-    return `(${result})`;
-  }
-  isEmpty() {
-    return this.len() === 0;
-  }
-  /** usage: `for of` */
-  items(): Generator<CrDataValue> {
-    if (this.arrayMode) {
-      return sliceGenerator(this.arrayValue, this.arrayStart, this.arrayEnd);
-    } else {
-      return listToItems(this.value);
-    }
-  }
-  append(v: CrDataValue) {
-    this.turnListMode();
-    return new CrDataList(ternaryTree.append(this.value, v));
-  }
-  prepend(v: CrDataValue) {
-    this.turnListMode();
-    return new CrDataList(ternaryTree.prepend(this.value, v));
-  }
-  first() {
-    if (this.arrayMode) {
-      if (this.arrayValue.length > this.arrayStart) {
-        return this.arrayValue[this.arrayStart];
-      } else {
-        return null;
-      }
-    } else {
-      return ternaryTree.first(this.value);
-    }
-  }
-  rest() {
-    if (this.arrayMode) {
-      return this.slice(1, this.arrayEnd - this.arrayStart);
-    } else {
-      return new CrDataList(ternaryTree.rest(this.value));
-    }
-  }
-  concat(ys: CrDataList) {
-    if (!(ys instanceof CrDataList)) {
-      throw new Error("Expected list");
-    }
-    this.turnListMode();
-    ys.turnListMode();
-    return new CrDataList(ternaryTree.concat(this.value, ys.value));
-  }
-  map(f: (v: CrDataValue) => CrDataValue) {
-    let result: Array<CrDataValue> = [];
-    for (let item of this.items()) {
-      result.push(f(item));
-    }
-    return new CrDataList(result);
-  }
-  toArray(): CrDataValue[] {
-    if (this.arrayMode) {
-      return this.arrayValue.slice(this.arrayStart, this.arrayEnd);
-    } else {
-      return [...ternaryTree.listToItems(this.value)];
-    }
-  }
-  reverse() {
-    this.turnListMode();
-    return new CrDataList(ternaryTree.reverse(this.value));
-  }
-}
-
-type MapChain = {
-  value: TernaryTreeMap<CrDataValue, CrDataValue>;
-  next: MapChain | null;
-};
-
-// just create a reference that equals to no other value
-let fakeUniqueSymbol = [] as any;
-
-class CrDataMap {
-  cachedHash: Hash;
-  chain: MapChain;
-  depth: number;
-  skipValue: CrDataValue;
-  constructor(value: TernaryTreeMap<CrDataValue, CrDataValue>) {
-    this.chain = { value: value, next: null };
-    this.depth = 1;
-    this.skipValue = fakeUniqueSymbol;
-  }
-  turnSingleMap() {
-    if (this.depth === 1) {
-      return;
-    }
-    // squash down to a single level of map
-    let ret = this.chain.value;
-    let cursor = this.chain.next;
-    while (cursor != null) {
-      if (!isMapEmpty(cursor.value)) {
-        ret = ternaryTree.mergeSkip(cursor.value, ret, this.skipValue);
-      }
-      cursor = cursor.next;
-    }
-    this.chain = {
-      value: ret,
-      next: null,
-    };
-    this.depth = 1;
-  }
-  len() {
-    this.turnSingleMap();
-    return mapLen(this.chain.value);
-  }
-  get(k: CrDataValue) {
-    let cursor = this.chain;
-    while (cursor != null) {
-      let v = mapGet(cursor.value, k);
-      if (v != null && v !== this.skipValue) {
-        return v;
-      } else {
-        cursor = cursor.next;
-      }
-    }
-    return null;
-  }
-  assoc(k: CrDataValue, v: CrDataValue) {
-    let cursor = this.chain;
-    // mutable way of creating another map value
-    let result = new CrDataMap(null);
-    result.chain = {
-      value: assocMap(cursor.value, k, v),
-      next: cursor.next,
-    };
-    return result;
-  }
-  dissoc(k: CrDataValue) {
-    this.turnSingleMap();
-    return new CrDataMap(dissocMap(this.chain.value, k));
-  }
-  toString() {
-    let itemsCode = "";
-    for (let [k, v] of this.pairs()) {
-      if (itemsCode !== "") {
-        itemsCode = `${itemsCode}, `;
-      }
-      itemsCode = `${itemsCode}${toString(k, true)} ${toString(v, true)}`;
-    }
-    return `{${itemsCode}}`;
-  }
-  isEmpty() {
-    let cursor = this.chain;
-    while (cursor != null) {
-      if (!isMapEmpty(cursor.value)) {
-        return false;
-      }
-      cursor = cursor.next;
-    }
-    return true;
-  }
-  pairs(): Array<[CrDataValue, CrDataValue]> {
-    this.turnSingleMap();
-    return toPairsArray(this.chain.value);
-  }
-  contains(k: CrDataValue) {
-    let cursor = this.chain;
-    while (cursor != null) {
-      if (ternaryTree.contains(cursor.value, k)) {
-        return true;
-      }
-      cursor = cursor.next;
-    }
-    return false;
-  }
-  merge(ys: CrDataMap) {
-    return this.mergeSkip(ys, fakeUniqueSymbol);
-  }
-  mergeSkip(ys: CrDataMap, v: CrDataValue) {
-    if (!(ys instanceof CrDataMap)) {
-      throw new Error("Expected map");
-    }
-
-    let result = new CrDataMap(null);
-    result.skipValue = v;
-    ys.turnSingleMap();
-    result.chain = {
-      value: ys.chain.value,
-      next: this.chain,
-    };
-    result.depth = this.depth + 1;
-    if (result.depth > 5) {
-      // 5 by experience, limit to suqash linked list to value
-      result.turnSingleMap();
-    }
-    return result;
-  }
-}
-
-type CrDataValue =
-  | string
-  | number
-  | boolean
-  | CrDataMap
-  | CrDataList
-  // TODO set
-  | Set<CrDataValue>
-  | CrDataKeyword
-  | CrDataSymbol
-  | CrDataAtom
-  | CrDataFn
-  | CrDataRecur // should not be exposed to function
-  | null;
-
-var keywordRegistery: Record<string, CrDataKeyword> = {};
-
-export let kwd = (content: string) => {
-  let item = keywordRegistery[content];
-  if (item != null) {
-    return item;
-  } else {
-    let v = new CrDataKeyword(content);
-    keywordRegistery[content] = v;
-    return v;
-  }
-};
-
-var atomsRegistry = new Map<string, CrDataAtom>();
-
-function* sliceGenerator(
-  xs: Array<CrDataValue>,
-  start: number,
-  end: number
-): Generator<CrDataValue> {
-  for (let idx = start; idx < end; idx++) {
-    yield xs[idx];
-  }
-}
 
 export let type_DASH_of = (x: any): CrDataKeyword => {
   if (typeof x === "string") {
@@ -1453,53 +1083,6 @@ export let turn_DASH_symbol = (x: CrDataValue): CrDataKeyword => {
   throw new Error("Unexpected data for symbol");
 };
 
-let toString = (x: CrDataValue, escaped: boolean): string => {
-  if (x == null) {
-    return "nil";
-  }
-  if (typeof x === "string") {
-    if (escaped) {
-      return JSON.stringify(x);
-    } else {
-      return x;
-    }
-  }
-  if (typeof x === "number") {
-    return x.toString();
-  }
-  if (typeof x === "boolean") {
-    return x.toString();
-  }
-  if (typeof x === "function") {
-    return `(&fn ...)`;
-  }
-  if (x instanceof CrDataSymbol) {
-    return x.toString();
-  }
-  if (x instanceof CrDataKeyword) {
-    return x.toString();
-  }
-  if (x instanceof CrDataList) {
-    return x.toString();
-  }
-  if (x instanceof CrDataMap) {
-    return x.toString();
-  }
-  if (x instanceof Set) {
-    let itemsCode = "";
-    x.forEach((child, idx) => {
-      if (idx > 0) {
-        itemsCode = `${itemsCode} `;
-      }
-      itemsCode = `${itemsCode}${toString(child, true)}`;
-    });
-    return `#{${itemsCode}}`;
-  }
-
-  console.error(x);
-  throw new Error("Unexpected data for toString");
-};
-
 export let pr_DASH_str = (...args: CrDataValue[]): string => {
   return args.map((x) => toString(x, true)).join(" ");
 };
@@ -1700,93 +1283,6 @@ export let listToArray = (xs: CrDataList): Array<CrDataValue> => {
   }
 };
 
-let defaultHash_nil = valueHash("nil:");
-let defaultHash_number = valueHash("number:");
-let defaultHash_string = valueHash("string:");
-let defaultHash_keyword = valueHash("keyword:");
-let defaultHash_true = valueHash("true:");
-let defaultHash_false = valueHash("false:");
-let defaultHash_symbol = valueHash("symbol:");
-let defaultHash_fn = valueHash("fn:");
-let defaultHash_atom = valueHash("atom:");
-let defaultHash_set = valueHash("set:");
-let defaultHash_list = valueHash("list:");
-let defaultHash_map = valueHash("map:");
-
-let fnHashCounter = 0;
-
-let hashFunction = (x: CrDataValue): Hash => {
-  if (x == null) {
-    return defaultHash_nil;
-  }
-  if (typeof x === "number") {
-    return mergeValueHash(defaultHash_number, x);
-  }
-  if (typeof x === "string") {
-    return mergeValueHash(defaultHash_string, x);
-  }
-  // dirty solution of caching, trying to reduce cost
-  if ((x as any).cachedHash != null) {
-    return (x as any).cachedHash;
-  }
-  if (x instanceof CrDataKeyword) {
-    let h = mergeValueHash(defaultHash_keyword, x.value);
-    x.cachedHash = h;
-    return h;
-  }
-  if (x === true) {
-    return defaultHash_true;
-  }
-  if (x === false) {
-    return defaultHash_false;
-  }
-  if (x instanceof CrDataSymbol) {
-    let h = mergeValueHash(defaultHash_symbol, x.value);
-    x.cachedHash = h;
-    return h;
-  }
-  if (typeof x === "function") {
-    fnHashCounter = fnHashCounter + 1;
-    let h = mergeValueHash(defaultHash_fn, fnHashCounter);
-    (x as any).cachedHash = h;
-    return h;
-  }
-  if (x instanceof CrDataAtom) {
-    let h = mergeValueHash(defaultHash_atom, x.path);
-    x.cachedHash = h;
-    return h;
-  }
-  if (x instanceof Set) {
-    // TODO not using dirty solution for code
-    let base = defaultHash_set;
-    for (let item of x) {
-      base = mergeValueHash(base, hashFunction(item));
-    }
-    return base;
-  }
-  if (x instanceof CrDataList) {
-    let base = defaultHash_list;
-    for (let item of x.items()) {
-      base = mergeValueHash(base, hashFunction(item));
-    }
-    x.cachedHash = base;
-    return base;
-  }
-  if (x instanceof CrDataMap) {
-    let base = defaultHash_map;
-    for (let [k, v] of x.pairs()) {
-      base = mergeValueHash(base, hashFunction(k));
-      base = mergeValueHash(base, hashFunction(v));
-    }
-    x.cachedHash = base;
-    return base;
-  }
-  throw new Error("Unknown data for hashing");
-};
-
-// Dirty code to change ternary-tree behavior
-overwriteHashGenerator(hashFunction);
-
 export let number_QUES_ = (x: CrDataValue): boolean => {
   return typeof x === "number";
 };
@@ -1826,21 +1322,22 @@ let placeholder = (...xs: []) => {
   /* placeholder */
 };
 
-// TODO these functions are not used by calcit-js, but added to fix missing imports
+export let escape = placeholder; // TODO
+export let format_DASH_time = placeholder; // TODO
+export let now_BANG_ = placeholder; // TODO
+export let parse_DASH_cirru = placeholder; // TODO
+export let parse_DASH_cirru_DASH_edn = placeholder; // TODO
+export let parse_DASH_time = placeholder; // TODO
+export let read_DASH_file = placeholder; // TODO Node.js
+export let write_DASH_file = placeholder; // TODO Node.js
+
+// these functions are available for by calcit-js
 export let _AND_reset_DASH_gensym_DASH_index_BANG_ = placeholder;
 export let assoc_DASH_after = placeholder;
 export let assoc_DASH_before = placeholder;
 export let dbt_DASH__GT_point = placeholder;
 export let dbt_DASH_digits = placeholder;
 export let dual_DASH_balanced_DASH_ternary = placeholder;
-export let escape = placeholder;
-export let format_DASH_time = placeholder;
 export let gensym = placeholder;
 export let macroexpand = placeholder;
 export let macroexpand_DASH_all = placeholder;
-export let now_BANG_ = placeholder;
-export let parse_DASH_cirru = placeholder;
-export let parse_DASH_cirru_DASH_edn = placeholder;
-export let parse_DASH_time = placeholder;
-export let read_DASH_file = placeholder;
-export let write_DASH_file = placeholder;
